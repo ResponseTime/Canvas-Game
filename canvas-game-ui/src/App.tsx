ff12cbc3c94b93e05de86d1f920278f899d07300
responsetime <aayush.bhattacharjee2002@gmail.com>
2025-02-24 19:37:02 +0530

    (fix) fixed clear canvas bug where old paths were drawn for ws users and fixed first click msgs not sent to ws users

--- canvas-game-ui/src/App.tsx
+++ canvas-game-ui/src/App.tsx
@@ -6,19 +6,17 @@ function App() {
   const [globalCtx, setGlobalCtx] = useState<null | CanvasRenderingContext2D>(null)
   const [isDrawing, setIsDrawing] = useState<boolean>(false)
   const [socket, setSocket] = useState<WebSocket | null>(null)
+
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:3000/ws")
     setSocket(ws)
-    ws.onopen = () => {
-      ws.send("hello world")
-    }
     return () => {
       if (ws.readyState === WebSocket.OPEN) {
         ws.close();
       }
     }
   }, [])
-
+  const [disableCanvas, setDisableCanvas] = useState<boolean>(false)
   useEffect(() => {
     if (socket) {
       socket.onmessage = (msg) => {
@@ -30,8 +28,14 @@ function App() {
             console.log(msg.data + "1")
             const [x, y] = msg.data.split(",").map(Number)
             globalCtx.moveTo(x, y)
-          } else {
+          } else if (msg.data === "disable") {
             console.log(msg.data + "2")
+            setDisableCanvas(true)
+          } else if (msg.data === "enable") {
+            console.log(msg.data + "3")
+            globalCtx.beginPath()
+          } else {
+            console.log(msg.data + "4")
             const [x, y] = msg.data.split(",").map(Number)
             globalCtx.lineTo(x, y);
             globalCtx.stroke()
@@ -54,10 +58,16 @@ function App() {
     ctx.strokeStyle = "red";
 
     const start = (event: MouseEvent) => {
+      if (!socket || socket.readyState !== WebSocket.OPEN) {
+        console.warn("WebSocket is not open yet");
+        return;
+      }
       ctx.beginPath()
       ctx.moveTo(event.offsetX, event.offsetY)
+      socket?.send("disable")
       socket?.send([event.offsetX, event.offsetY].toString() + ",start")
-      setIsDrawing(true)
+      setIsDrawing(true);
+      console.log("ws 1 2")
     }
     const draw = (event: MouseEvent) => {
       if (!isDrawing) return;
@@ -67,7 +77,8 @@ function App() {
     }
 
     const stop = () => {
-      setIsDrawing(false)
+      socket?.send("enable")
+      setIsDrawing(false);
       ctx.beginPath()
     }
 
@@ -80,16 +91,21 @@ function App() {
       canvas.removeEventListener("mousemove", draw)
       canvas.removeEventListener("mouseup", stop)
     }
-  }, [isDrawing])
+  }, [isDrawing, socket])
+
   function handleClear() {
     if (globalCtx && canvasRef.current) {
-      globalCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+      if (!socket || socket.readyState !== WebSocket.OPEN) {
+        console.warn("WebSocket is not open yet");
+        return;
+      }
       socket?.send("clear")
+      globalCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
     }
   }
   return (
     <>
-      <canvas ref={canvasRef} width="600" height="600"></canvas>
+      <canvas ref={canvasRef} width="600" height="600" style={{ pointerEvents: disableCanvas ? "none" : undefined }}></canvas>
       <button onClick={handleClear}>Clear Canvas</button>
     </>
   )
